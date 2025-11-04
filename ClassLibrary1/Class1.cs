67a2c4f0eddaafc91b297092c2ca28f08c0f7829
Jaas <jasserfridhi08@gmail.com>
2025-11-04 22:58:38 +0100

    Aggiungere i file di progetto.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,117 @@
+﻿namespace ClassLibrary1
+{
+     public  class NumeroComplesso
+    {
+        public int R { get; set; }
+        public int IMG { get; set; }
+
+        public NumeroComplesso(int parteReale, int parteImmaginaria)
+        {
+            R = parteReale;
+            IMG = parteImmaginaria;
+        }
+
+        public static NumeroComplesso Parse(string s)
+        {
+           
+            string t = s.Trim().Replace(" ", "");
+            if (t.Length == 0)
+                throw new Exception("La stringa non può essere vuota");
+
+            bool isImmaginario = t.EndsWith("i", StringComparison.OrdinalIgnoreCase);
+            int reale = 0;
+            int immaginaria = 0;
+
+            if (isImmaginario)
+            {
+                string core = t.Substring(0, t.Length - 1);         // toglie la "i" finale
+
+                if (string.IsNullOrEmpty(core))
+                {
+                    immaginaria = 1;                                // "i"
+                    return new NumeroComplesso(0, immaginaria);
+                }
+                if (core == "+")
+                {
+                    immaginaria = 1;
+                    return new NumeroComplesso(0, immaginaria);
+                }
+                if (core == "-")                                    // "-i"
+                {
+                    immaginaria = -1;
+                    return new NumeroComplesso(0, immaginaria);
+                }
+                int dividiRealeImmaginaria = -1;
+                for (int i = core.Length - 1; i >= 1; i--)          //trova l'inizio della parte immaginaria
+                {
+                    char c = core[i];
+                    if (c == '+' || c == '-')
+                    {
+                        dividiRealeImmaginaria = i;
+                        break;
+                    }
+                }
+                string realeStringa = null;
+                string immaginariaString = null;
+
+                if (dividiRealeImmaginaria > 0)
+                {
+                    realeStringa = core.Substring(0, dividiRealeImmaginaria);
+                    immaginariaString = core.Substring(dividiRealeImmaginaria);
+                }
+                else
+                {
+                    immaginariaString = core;               // solo parte immaginaria
+                }
+                if (string.IsNullOrEmpty(immaginariaString))
+                    throw new Exception("parte immaginaria non valida");
+                if (immaginariaString == "+")
+                    immaginaria = 1;
+                else if (immaginariaString == "-")
+                    immaginaria = -1;
+                else
+                {
+                    if (!int.TryParse(immaginariaString, out immaginaria))
+                        throw new Exception($"Parte immaginaria non valida: '{immaginariaString}'.");
+                }
+                if (!string.IsNullOrEmpty(realeStringa))
+                {
+                    if (!int.TryParse(realeStringa, out reale))
+                        throw new Exception($"Parte reale non valida: '{realeStringa}'.");
+                }
+                return new NumeroComplesso(reale, immaginaria);
+            }
+            else
+            {
+                if (!int.TryParse(t, out reale))                                    // solo parte reale
+                    throw new Exception($"Parte reale non valida: '{t}'.");
+                return new NumeroComplesso(reale, 0);
+            }
+        }
+
+
+      
+        public static NumeroComplesso operator +(NumeroComplesso a, NumeroComplesso b)
+        {
+            return new NumeroComplesso(a.R + b.R, a.IMG + b.IMG);
+        }
+        public static NumeroComplesso operator -(NumeroComplesso a, NumeroComplesso b)
+        {
+            return new NumeroComplesso(a.R - b.R, a.IMG - b.IMG);
+        }
+        public static NumeroComplesso operator *(NumeroComplesso a, NumeroComplesso b)
+        {
+            int R = a.R * b.R - a.IMG * b.IMG;
+            int parteImmaginaria = a.R * b.IMG + a.IMG * b.R;
+            return new NumeroComplesso(R, parteImmaginaria);
+        }
+        public static NumeroComplesso operator /(NumeroComplesso a, NumeroComplesso b)
+        {
+            int denominatore = b.R * b.R + b.IMG * b.IMG;
+            int r = (a.R * b.R + a.IMG * b.IMG) / denominatore;
+            int img = (a.IMG * b.R - a.R * b.IMG) / denominatore;
+            return new NumeroComplesso(r, img);
+        }
+
+    }
+}
---
+++ NUMCOMPLESSO/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("inserisci primo num complesso");
+        NumeroComplesso c1 = NumeroComplesso.Parse(Console.ReadLine());
+        Console.WriteLine("inserisci secondo num complesso");
+        NumeroComplesso c2 = NumeroComplesso.Parse(Console.ReadLine());
+        var somma = c1 + c2;
+        Console.WriteLine($"{somma}");
+        var sottr = c1 - c2;
+        Console.WriteLine($"{sottr}");
+        var molt = c1 * c2;
+        Console.WriteLine($"{molt}");
+        var diviso = c1 / c2;
+        Console.WriteLine($"{diviso}");
+    }
+}

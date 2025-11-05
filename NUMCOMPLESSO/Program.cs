642aa54f1c6fd16e12814bae300487b73a25ee84
Jasss <153324785+Jassss7@users.noreply.github.com>
2025-11-05 08:14:38 +0100

    Update Program.cs

--- NUMCOMPLESSO/Program.cs
+++ NUMCOMPLESSO/Program.cs
@@ -9,12 +9,12 @@ class Program
         Console.WriteLine("inserisci secondo num complesso");
         NumeroComplesso c2 = NumeroComplesso.Parse(Console.ReadLine());
         var somma = c1 + c2;
-        Console.WriteLine($"{somma}");
+        Console.WriteLine($"{somma.R},{somma.IMG");
         var sottr = c1 - c2;
-        Console.WriteLine($"{sottr}");
+        Console.WriteLine($"{sottr.R},{sottr.IMG");
         var molt = c1 * c2;
-        Console.WriteLine($"{molt}");
+        Console.WriteLine($"{molt.R},{molt.IMG}");
         var diviso = c1 / c2;
-        Console.WriteLine($"{diviso}");
+        Console.WriteLine($"{diviso.R},{diviso.IMG}");
     }
 }
